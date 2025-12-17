0662182647315bba81e36af5d4a730ec62f9c6a0
Sticka159 <steamucetkrony@seznam.cz>
2025-12-16 23:26:59 -0800

    final cloud 3 FE url

--- src/frontend/src/services/authService.js
+++ src/frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5001/auth";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5001";
 
 export async function login(email, password) {
   const response = await axios.post(`${API_URL}/login`, { email, password });
