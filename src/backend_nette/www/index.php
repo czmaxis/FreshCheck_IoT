081aef21d76c3efff193a1fa513f7a714f40d778
czmaxis <czmaxis55@gmail.com>
2026-02-09 22:35:04 +0100

    docker file implemented

--- src/backend_nette/www/index.php
+++ src/backend_nette/www/index.php
@@ -6,7 +6,8 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use App\Bootstrap;
 // ===== CORS =====
-header('Access-Control-Allow-Origin: http://localhost:3000');
+$allowedOrigin = $_ENV['CORS_ORIGIN'] ?? 'http://localhost:3000';
+header('Access-Control-Allow-Origin: ' . $allowedOrigin);
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
