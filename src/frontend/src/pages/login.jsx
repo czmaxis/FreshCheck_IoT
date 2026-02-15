319ad49aaecad5815346fba83c1b1c99e80fb00a
czmaxis <czmaxis55@gmail.com>
2026-02-15 20:24:25 +0100

    login comment change

--- src/frontend/src/pages/login.jsx
+++ src/frontend/src/pages/login.jsx
@@ -240,9 +240,9 @@ export default function Login() {
               color="text.secondary"
               sx={{ fontFamily: "monospace" }}
             >
-              Email: nette@example.com
+              Email: demo@example.com
               <br />
-              Heslo: 123456
+              Heslo: demo123
             </Typography>
           </Box>
         </CardContent>
